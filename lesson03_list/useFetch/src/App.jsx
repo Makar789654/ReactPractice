2cdff219a8d3c43f0e6fd8c97380a4993cd945a3
Makar789654 <112611445+Makar789654@users.noreply.github.com>
2023-02-24 15:27:49 +0100

    just for fun

--- lesson03_list/useFetch/src/App.jsx
+++ lesson03_list/useFetch/src/App.jsx
@@ -1,13 +1,48 @@
 import useFetch from "react-fetch-hook";
+import './style.css'
+import { useState } from "react";
+
 
 export default function App() {
-    const {isLoading, data, error } = useFetch("https://rickandmortyapi.com/api/character?page=2");
+  const [page, setPage] = useState(1);
+  const { isLoading, data, error } = useFetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  );
 
-    if(error) {console.log(error)}
+  if (error) {
+    console.log(error);
+  }
 
-    const newRick = data?.results.map((item) => (
-        <li key={item.id}>{item.name}</li>
+  const handleClickNext = () => {
+    setPage(page + 1);
+  };
+
+  const handleClickPrevious = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const newRick =
+    data?.results.map((item) => (
+      <div className="characters" key={item.id}>
+        <li>
+          <img src={item.image} />
+          {item.name} {item.gender}
+        </li>
+      </div>
     ));
 
-    return isLoading ? <div>...Loading</div> : <ul>{newRick}</ul>
+  return (
+    <>
+      <ul>{newRick}</ul>
+      <button className="previous-page" onClick={handleClickPrevious}>
+        Previous page
+      </button>
+      <button className="next-page" onClick={handleClickNext}>
+        Next page
+      </button>
+      {isLoading && <div>...Loading</div>}
+    </>
+  );
 }
--- lesson03_list/useFetch/src/index.js
+++ lesson03_list/useFetch/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Rick and Morty</h1>
     <App />
   </React.StrictMode>
 );
-
 
